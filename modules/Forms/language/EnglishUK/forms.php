7fdba44cbc199a618468810f763841869adaba0c
Jona <kontakt@jonafeucht.de>
2019-05-20 20:17:34 +0200

    module upload

---
+++ modules/Forms/language/EnglishUK/forms.php
@@ -0,0 +1,102 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language for Forms module
+ */
+
+$language = array(
+	// Forms
+	'forms' => 'Forms',
+	'form' => 'Form',
+	'new_form' => 'New Form',
+	'form_name' => 'Form Name',
+	'form_url' => 'Form URL (with preceding /, eg /example)',
+	'form_icon' => 'Form Icon',
+	'link_location' => 'Link Location',
+	'creating_new_form' => 'Creating New Form',
+	'form_created_successfully' => 'Form created successfully',
+	'none_forms_defined' => 'There are no forms yet.',
+	'delete_form' => 'Are you sure you want to delete this form?</br>Warning: All data who belongs to this form will be deleted like questions and submissions',
+	'form_submitted' => 'Form submitted successfully',
+	'action' => 'Action',
+	'actions' => 'Actions',
+	'guest' => 'Guest',
+	
+	// Permissions
+	'forms_view_submissions' => 'StaffCP &raquo; Forms &raquo; Submissions',
+	'forms_manage' => 'StaffCP &raquo; Forms &raquo; Forms',
+	
+	// Form
+	'editing_x' => 'Editing {x}', // Don't replace {x}
+	'form_created_successfully' => 'Form created successfully.',
+	'form_updated_successfully' => 'Form updated successfully.',
+	'form_deleted_successfully' => 'Form deleted successfully.',
+	'allow_guests' => 'Can guests visit this form?',
+	'allow_guests_help' => 'Guests will be able to send in submissions without being logged in, Please note them won\'t be able to view the submission afterwards',
+	'can_user_view' => 'Can user view his own submission?',
+	'can_user_view_help' => 'User will be able to view his own submission and use the comment section, User will also receive update alerts when the status change or when someone comment, Please note this won\'t work for guests.',
+	
+	// Fields
+	'field' => 'Field',
+	'fields' => 'Fields',
+	'new_field' => 'New Field',
+	'field_name' => 'Field Name',
+	'field_created_successfully' => 'Field created successfully',
+	'field_updated_successfully' => 'Field updated successfully',
+	'field_deleted_successfully' => 'Field deleted successfully',
+	'new_field_for_x' => 'Creating new field for {x}',
+	'editing_field_for_x' => 'Editing field for {x}',
+	'none_fields_defined' => 'There are no fields yet.',
+	'confirm_delete_field' => 'Are you sure you want to delete this field?',
+	'options' => 'Options',
+	'options_help' => 'Each option on a new line; can be left empty (options only)',
+	'field_order' => 'Field Order',
+	'delete_field' => 'Are you sure you want to delete this field?',
+	
+	// Statuses
+	'statuses' => 'statuses',
+	'status' => 'Status',
+	'new_status' => 'New Status',
+	'creating_status' => 'Creating new status',
+	'editing_status' => 'Editing status',
+	'marked_as_open' => 'Marked as open',
+	'status_name' => 'Status Name',
+	'status_html' => 'Status HTML',
+	'status_forms' => 'Status Forms',
+	'status_groups' => 'Status Groups',
+	'status_creation_success' => 'Status created successfully.',
+	'status_creation_error' => 'Error creating a status. Please ensure the status html is no longer than 1024 characters.',
+	'status_edit_success' => 'Status successfully edited.',
+	'status_deleted_successfully' => 'Status deleted successfully.',
+	'delete_status' => 'Are you sure you want to delete this status?',
+
+	// Errors
+	'input_form_name' => 'Please input a form name.',
+	'input_form_url' => 'Please input a form url.',
+	'form_name_minimum' => 'The form name must be a minimum of 2 characters.',
+	'form_url_minimum' => 'The form url must be a minimum of 2 characters.',
+	'form_name_maximum' => 'The form name must be a maximum of 32 characters.',
+	'form_url_maximum' => 'The form url must be a maximum of 32 characters.',
+	'form_icon_maximum' => 'The form icon must be a maximum of 64 characters.',
+	'input_field_name' => 'Please input a field name.',
+	'field_name_minimum' => 'The field name must be a minimum of 2 characters.',
+	'field_name_maximum' => 'The field name must be a maximum of 255 characters.',
+	
+	// Submissions
+	'submissions' => 'Submissions',
+	'submission_updated' => 'Submission updated successfully',
+	'no_open_submissions' => 'There are currently no open submissions.',
+	'no_closed_submissions' => 'There are currently no closed submissions.',
+	'form_x' => 'Form: {x}',
+	'current_status_x' => 'Current status: {x}',
+	'last_updated' => 'Last updated:',
+	'your_submission_updated' => 'Your submission has been updated',
+	'user' => 'User',
+	'updated_by' => 'Updated by',
+);
---
+++ modules/Forms/module.php
@@ -0,0 +1,214 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Forms module file
+ */
+
+class Forms_Module extends Module {
+	private $_language;
+	private $_forms_language;
+	private $_cache;
+
+	public function __construct($language, $forms_language, $pages, $queries, $navigation, $cache){
+		$this->_language = $language;
+		$this->_forms_language = $forms_language;
+		$this->_cache = $cache;
+
+		$name = 'Forms';
+		$author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a>';
+		$module_version = '1.0.0';
+		$nameless_version = '2.0.0-pr6';
+
+		parent::__construct($this, $name, $author, $module_version, $nameless_version);
+
+		// Define URLs which belong to this module
+		$pages->add('Forms', '/panel/form', 'pages/panel/form.php');
+		$pages->add('Forms', '/panel/forms', 'pages/panel/forms.php');
+		$pages->add('Forms', '/panel/forms/statuses', 'pages/panel/statuses.php');
+		$pages->add('Forms', '/panel/forms/submissions', 'pages/panel/submissions.php');
+		$pages->add('Forms', '/user/submissions', 'pages/user/submissions.php');
+		
+		// is installed
+		$cache->setCache('forms');
+		if($cache->isCached('forms_installed')){
+			$forms = $queries->getWhere('forms', array('id', '<>', 0));
+			if(count($forms)){
+				foreach($forms as $form){
+					// Register form page
+					$pages->add('Forms', $form->url, 'pages/form.php', 'form-' . $form->id, true);
+					
+					// Add link location
+					switch($form->link_location){
+						case 1:
+							// Navbar
+							// Check cache first
+							$cache->setCache('navbar_order');
+							if(!$cache->isCached('form-' . $form->id . '_order')){
+								// Create cache entry now
+								$form_order = 5;
+								$cache->store('form-' . $form->id . '_order', 5);
+							} else {
+								$form_order = $cache->retrieve('form-' . $form->id . '_order');
+							}
+							$navigation->add('form-' . $form->id, Output::getClean($form->title), URL::build(Output::getClean($form->url)), 'top', null, $form_order, $form->icon);
+						break;
+						case 2:
+							// "More" dropdown
+							$navigation->addItemToDropdown('more_dropdown', 'form-' . $form->id, Output::getClean($form->title), URL::build(Output::getClean($form->url)), 'top', null, $form->icon);
+						break;
+						case 3:
+							// Footer
+							$navigation->add('form-' . $form->id, Output::getClean($form->title), URL::build(Output::getClean($form->url)), 'footer', null, 2000, $form->icon);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	public function onInstall(){
+		// Queries
+		$queries = new Queries();
+		
+		try {
+			// Create tabels
+			$data = $queries->createTable("forms", " `id` int(11) NOT NULL AUTO_INCREMENT, `url` varchar(32) NOT NULL, `title` varchar(32) NOT NULL, `guest` tinyint(1) NOT NULL DEFAULT '0', `link_location` tinyint(1) NOT NULL DEFAULT '1', `icon` varchar(64) NULL, `can_view` tinyint(1) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			$data = $queries->createTable("forms_comments", " `id` int(11) NOT NULL AUTO_INCREMENT, `form_id` int(11) NOT NULL, `user_id` int(11) NOT NULL, `created` int(11) NOT NULL, `content` mediumtext NOT NULL, PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			$data = $queries->createTable("forms_fields", " `id` int(11) NOT NULL AUTO_INCREMENT, `form_id` int(11) NOT NULL, `name` varchar(255) NOT NULL, `type` int(11) NOT NULL, `required` tinyint(1) NOT NULL DEFAULT '0', `options` text NULL, `deleted` tinyint(1) NOT NULL DEFAULT '0', `order` int(11) NOT NULL DEFAULT '1', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			$data = $queries->createTable("forms_replies", " `id` int(11) NOT NULL AUTO_INCREMENT, `form_id` int(11) NOT NULL, `user_id` int(11) NULL, `updated_by` int(11) NULL, `created` int(11) NOT NULL, `updated` int(11) NOT NULL, `content` mediumtext NOT NULL, `status_id` int(11) NOT NULL DEFAULT '1', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			$data = $queries->createTable("forms_statuses", " `id` int(11) NOT NULL AUTO_INCREMENT, `html` varchar(1024) NOT NULL, `open` tinyint(1) NOT NULL, `fids` varchar(128) NULL, `gids` varchar(128) NULL, `deleted` tinyint(1) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=latin1");
+		} catch(Exception $e){
+			// Error
+		}
+		
+		try {
+			// Insert data
+			$queries->create('forms_statuses', array(
+				'html' => '<span class="badge badge-success">Open</span>',
+				'open' => 1,
+				'fids' => '1',
+				'gids' => '2,3'
+			));
+			$queries->create('forms_statuses', array(
+				'html' => '<span class="badge badge-danger">Closed</span>',
+				'open' => 0,
+				'fids' => '1',
+				'gids' => '2,3'
+			));
+			$queries->create('forms_statuses', array(
+				'html' => '<span class="badge badge-warning">Under Considering</span>',
+				'open' => 1,
+				'fids' => '1',
+				'gids' => '2,3'
+			));
+			
+			// create example staff applications
+			$queries->create('forms', array(
+				'url' => '/apply',
+				'title' => 'Staff Applications',
+				'guest' => 0,
+				'link_location' => 1
+				
+			));
+			$queries->create('forms_fields', array(
+				'form_id' => 1,
+				'name' => 'Minecraft Name',
+				'type' => 1,
+				'required' => 1,
+				'order' => 1
+			));
+			$queries->create('forms_fields', array(
+				'form_id' => 1,
+				'name' => 'Why you want to become staff?',
+				'type' => 3,
+				'required' => 1,
+				'order' => 2
+			));
+			
+		} catch(Exception $e){
+			// Error
+		}
+		
+		try {
+			// Update main admin group permissions
+			$group = $queries->getWhere('groups', array('id', '=', 2));
+			$group = $group[0];
+			
+			$group_permissions = json_decode($group->permissions, TRUE);
+			$group_permissions['forms.manage'] = 1;
+			$group_permissions['forms.view-submissions'] = 1;
+			$group_permissions['forms.manage-submission'] = 1;
+			
+			$group_permissions = json_encode($group_permissions);
+			$queries->update('groups', 2, array('permissions' => $group_permissions));
+		} catch(Exception $e){
+			// Error
+		}
+		
+		// Installed
+		$this->_cache->setCache('forms');
+		if(!$this->_cache->isCached('forms_installed')){
+			$this->_cache->store('forms_installed', true);
+		}
+	}
+
+	public function onUninstall(){
+
+	}
+
+	public function onEnable(){
+		// No actions necessary
+	}
+
+	public function onDisable(){
+		// No actions necessary
+	}
+
+	public function onPageLoad($user, $pages, $cache, $smarty, $navs, $widgets, $template){
+		// Permissions
+		PermissionHandler::registerPermissions('Forms', array(
+			'forms.view-submissions' => $this->_forms_language->get('forms', 'forms_view_submissions'),
+			'forms.manage' => $this->_forms_language->get('forms', 'forms_manage'),
+		));
+		
+		$navs[1]->add('cc_submissions', $this->_forms_language->get('forms', 'submissions'), URL::build('/user/submissions'));
+		
+		if(defined('BACK_END')){
+			if($user->hasPermission('forms.manage') || $user->hasPermission('forms.view-submissions')){
+				$cache->setCache('panel_sidebar');
+				if(!$cache->isCached('forms_order')){
+					$order = 12;
+					$cache->store('forms_order', 12);
+				} else {
+					$order = $cache->retrieve('forms_order');
+				}
+				$navs[2]->add('forms_divider', mb_strtoupper($this->_forms_language->get('forms', 'forms'), 'UTF-8'), 'divider', 'top', null, $order, '');
+				
+				if($user->hasPermission('forms.manage')){
+					if(!$cache->isCached('forms_icon')){
+						$icon = '<i class="nav-icon fas fa-cogs"></i>';
+						$cache->store('forms_icon', $icon);
+					} else {
+						$icon = $cache->retrieve('forms_icon');
+					}
+					$navs[2]->add('forms', $this->_forms_language->get('forms', 'forms'), URL::build('/panel/forms'), 'top', null, $order, $icon);
+				}
+				
+				if($user->hasPermission('forms.view-submissions')){
+					if(!$cache->isCached('forms_submissions_icon')){
+						$icon = '<i class="nav-icon fas fa-user-circle"></i>';
+						$cache->store('forms_submissions_icon', $icon);
+					} else {
+						$icon = $cache->retrieve('forms_submissions_icon');
+					}
+					$navs[2]->add('submissions', $this->_forms_language->get('forms', 'submissions'), URL::build('/panel/forms/submissions'), 'top', null, $order, $icon);
+				}
+			}
+		}
+	}
+}
