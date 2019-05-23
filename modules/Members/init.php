3358b983e3a1c7d6e3081f43a1a4025d801d5e8d
Jona <kontakt@jonafeucht.de>
2019-05-23 22:47:44 +0200

    Monarch depends on the friends and members module

---
+++ modules/Members/init.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+ *	Made by Partydragen And Samerton
+ *  https://github.com/partydragen/Members/
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Members initialisation file
+ */
+
+// Initialise forum language
+$members_language = new Language(ROOT_PATH . '/modules/Members/language', LANGUAGE);
+
+// Initialise module
+require_once(ROOT_PATH . '/modules/Members/module.php');
+$module = new Members_Module($members_language, $pages, $navigation, $cache);
---
+++ modules/Members/language/Czech/members.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ *	Made by Partydragen, translated by Renzotom
+ *  https://github.com/partydragen/Members/
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr5
+ *
+ *  License: MIT
+ *
+ *  Czech Language for Members module
+ */
+
+$language = array( 
+	'members' => 'Uživatelé',
+	'username' => 'Uživatelské jméno',
+	'group' => 'Skupina',
+	'created' => 'Vytvořeno',
+	'link_location' => 'Link Location',
+	'icon' => 'Icon',
+	'icon_example' => 'Example: <i class="fa fa-users" aria-hidden="true"></i>',
+	'all' => 'All'
+);
